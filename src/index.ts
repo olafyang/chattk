abfcb5d53be35e2baf7b788ca2e51a90a529b0c3
Olaf Yang <mail@olafyang.com>
2023-08-02 22:49:39 +0200

    build script + main package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
-export class TwitchClient {
-  static getChatClient() {}
+import { ChatClient as TwitchChatClient } from "./twitch/irc.js";
 
-  static getPubSubClient() {}
+export class TwitchClient extends EventTarget {
+  clientId: string;
+  token: string;
+  chatNickname: string;
+
+  twitchChatClient?: TwitchChatClient;
+
+  constructor(
+    clientID: string,
+    token: string,
+    chatNickname: string,
+    options = {
+      chat: true,
+      pubsub: true,
+    }
+  ) {
+    super();
+    this.clientId = clientID;
+    this.token = token;
+    this.chatNickname = chatNickname;
+  }
+
+  getChatClient(): TwitchChatClient {
+    if (this.twitchChatClient) {
+      return this.twitchChatClient;
+    }
+    this.twitchChatClient = new TwitchChatClient(
+      this.chatNickname,
+      `oauth:${this.token}`
+    );
+    return this.twitchChatClient;
+  }
 }
